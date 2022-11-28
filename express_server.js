02744ec73fa50551b79eecea707e84b8825e5285
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-27 19:14:30 -0500

    Add post req to save new URL and get req to redirect to long URL

--- express_server.js
+++ express_server.js
@@ -14,7 +14,7 @@ function generateRandomString() {
   return string;  
 }
 
-const urlDatabase = {
+let urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 }
@@ -32,8 +32,10 @@ app.get('/urls', (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const longURL = req.body.longURL;
+  const key = generateRandomString();
+  urlDatabase[key] = longURL;
+  res.redirect(`/urls/${key}`);
 });
 
 app.get('/urls/new', (req, res) => {
@@ -43,6 +45,11 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars)
+})
+
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 })
 
 app.listen(PORT, () => {
