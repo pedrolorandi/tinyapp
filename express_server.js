6505c748e88f27109af32e28ca9e508e5ec6224a
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-28 15:50:51 -0500

    Add login and logout logic

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const { render } = require('ejs');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
@@ -20,6 +21,7 @@ let urlDatabase = {
 }
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -27,7 +29,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']  
+  };
   res.render('urls_index', templateVars)
 });
 
@@ -39,7 +44,10 @@ app.post("/urls", (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new')
+  const templateVars = {
+    username: req.cookies['username']
+  }
+  res.render('urls_new', templateVars)
 })
 
 app.post('/urls/:id/delete', (req, res) => {
@@ -49,7 +57,11 @@ app.post('/urls/:id/delete', (req, res) => {
 })
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies['username'] 
+  };
   res.render(`urls_show`, templateVars);
 })
 
@@ -64,6 +76,17 @@ app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 })
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username)
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
